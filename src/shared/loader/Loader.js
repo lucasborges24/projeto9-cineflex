07865b92ccc2f6950daa9cf246eaafece7ad2179
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-05-17 11:15:50 -0300

    add Loader component

---
+++ src/shared/loader/Loader.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+export default function Loader() {
+    return (
+        <div class="spinner">
+            <div class="dot1"></div>
+            <div class="dot2"></div>
+            <div class="dot3"></div>
+        </div>
+    )
+}
