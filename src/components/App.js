90bf9903f1d7b6ae38a40a453c43e945700629eb
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-05-17 10:27:12 -0300

    create Head file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Movie from "./movie/Movie"
+import Head from "./Head/Head"
 
 export default function App() {
     return (
         <BrowserRouter>
+            <Head />
             <Routes>
                 <Route path="/" element={<Movie />} />
             </Routes>
---
+++ src/components/Head/Head.js
@@ -0,0 +1,5 @@
+export default function Head() {
+    return (
+        <p>head bolado</p>
+    )
+}
